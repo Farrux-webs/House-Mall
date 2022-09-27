89338f03ce189ec47369714ba5e6fb6aed35cbbd
RavFarrukh <ravshanbekovf02@gmail.com>
2022-09-27 06:59:29 -0700

    slider qoshildi

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-"use strict"
+const elsliderRight = document.querySelector(".sliderRight")
+const elsliderLeft = document.querySelector(".sliderLeft")
+const elheading = document.querySelector(".intro__wrapper__bottom__contents__heading")
+const elheader = document.querySelector(".intro")
 
-const prQuest = document.querySelector(".prQuest")
+
+elsliderRight.addEventListener("click", (e) => {
+    elheader.classList.toggle("intro__slider")
+    if(elheader.classList.contains("intro__slider")){
+        elheading.innerHTML = "Assalom Sohil house"
+    }else{
+        elheading.innerHTML = "Next level comfort houses"
+    }
+    
+})
+elsliderLeft.addEventListener("click", (e) => {
+    elheader.classList.toggle("intro__slider")
+    if(elheader.classList.contains("intro__slider")){
+        elheading.innerHTML = "Assalom Sohil house"
+    }else{
+        elheading.innerHTML = "Next level comfort houses"
+    }
+})
+
+
 
 
 
